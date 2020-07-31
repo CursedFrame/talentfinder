9c55eee92559c18b8677b84027723e85c3f41af7
CursedFrame <njp3003@att.net>
2020-07-31 17:13:55 -0500

    Change constraints of detail profile text views

--- app/src/main/java/com/example/talentfinder/fragments/CreateFragment.java
+++ app/src/main/java/com/example/talentfinder/fragments/CreateFragment.java
@@ -3,6 +3,7 @@ package com.example.talentfinder.fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.talentfinder.R;
 import com.example.talentfinder.activities.MainActivity;
 import com.example.talentfinder.databinding.FragmentCreateBinding;
@@ -70,9 +73,10 @@ public class CreateFragment extends MediaFragment implements AdapterView.OnItemS
         fragmentManager = getFragmentManager();
         context = getContext();
 
-        Glide.with(context)
-                .load(GlobalConstants.PLACEHOLDER_URL)
-                .into(binding.fragmentCreateProjectIvContext);
+//        Glide.with(context)
+//                .asFile()
+//                .load(GlobalConstants.PLACEHOLDER_URL)
+//                .into(binding.fragmentCreateProjectIvContext);
 
         // Array adapter for "Skill" spinner
         ArrayAdapter<CharSequence> spnSkillAdapter = ArrayAdapter.createFromResource(context, R.array.skill, R.layout.support_simple_spinner_dropdown_item);
@@ -105,6 +109,23 @@ public class CreateFragment extends MediaFragment implements AdapterView.OnItemS
                 project.setTitle(binding.fragmentCreateProjectEtProjectTitle.getText().toString());
                 if (photoFile != null) {
                     project.setImage(new ParseFile(photoFile));
+                }
+                else {
+                    Glide.with(context)
+                            .asFile()
+                            .load(GlobalConstants.PLACEHOLDER_URL)
+                            .into(new CustomTarget<File>() {
+                                @Override
+                                public void onResourceReady(@NonNull File resource, @Nullable Transition<? super File> transition) {
+                                    ParseFile projectPhoto = new ParseFile(resource);
+                                    project.setImage(projectPhoto);
+                                }
+
+                                @Override
+                                public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                                }
+                            });
                 }
                 project.setDescription(binding.fragmentCreateProjectEtProjectDescription.getText().toString());
                 project.setTalentTag(binding.fragmentCreateProjectSpnTalent.getSelectedItem().toString());
